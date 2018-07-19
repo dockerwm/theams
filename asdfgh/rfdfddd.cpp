e8a096fbcec839af44bef456a79d4b434a963da8
wangmiao <wangmiao@DESKTOP-Q826CTF>
2018-07-19 10:47:19 +0800

    sdfsdfsdfsddssddfsadfsa

---
+++ asdfgh/rfdfddd.cpp
@@ -0,0 +1,21 @@
+/*
+ * rfdfddd.cpp
+ *
+ *  Created on: 2018年7月14日
+ *      Author: wangmiao
+ */
+
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(void *){
+	printf("sdfsdfsfdsdf\n");
+	printf("sdsdsdsdffdsgggsdfsd\n");
+
+	cout << "asdfssdfdsfdsfsfd\n";
+	cout << "33sdfssdfdsfdsfsfd\n";
+	cout << "5rfdsdfssdfdsfdsfsfd\n";
+	return 0;
+}
+
